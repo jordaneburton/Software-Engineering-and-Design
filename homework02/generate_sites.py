b69598929a51a339b67ae17707c057b26cbbb417
Jordan Burton <jordane.burton@utexas.edu>
2023-01-28 17:50:03 -0600

    added files and partially wrote generate_sites

---
+++ homework02/generate_sites.py
@@ -0,0 +1,20 @@
+import random
+import math
+import json
+
+def generate_site(id_num): # generates a dictionary of properties for site
+    lat = 16 + 2*random.random()
+    lon = 82 + 2*random.random()
+    comp = random.choice(["stony", "iron", "stony-iron"])
+    return {
+            "site_id": id_num,
+            "latitude": lat,
+            "longitude": lon,
+            "composition": comp
+            }
+
+site_list = []
+for x in range(5): # generate 5 sites with random properties
+    site_list.append(generate_site(x+1))
+
+site_dict = {"sites": site_list}
