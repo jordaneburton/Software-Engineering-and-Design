0611db8afa46fe32572a62d9680ee220d3940523
Ubuntu <ubuntu@jeb5645-vm.js2local>
2023-04-21 03:23:49 +0000

    actually corrected hw7

--- homework07/gene_api.py
+++ homework07/gene_api.py
@@ -23,23 +23,23 @@ def data():
 
     if request.method == 'POST':
         data_url = 'https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/hgnc_complete_set.json'
-        response = requests.get(url)
+        response = requests.get(data_url)
         if response.status_code != 200:
-            return 'Error: Failed to retrieve data'
+            return 'Error: Failed to retrieve data\n'
         else:
             data = json.loads(response.text)
             rd.set('gene_data', json.dumps(data))
-            return 'Posted gene data'
+            return 'Posted gene data\n'
 
     elif request.method == 'GET':
         try:
             return json.loads(rd.get('gene_data').decode('utf-8'))
         except KeyError:
-            return "No data found. Use 'curl -X POST localhost:5000/data' to fetch data"
+            return "No data found. Use 'curl -X POST localhost:5000/data' to fetch data\n"
         
     elif request.method == 'DELETE':
         rd.delete('gene_data')
-        return 'Deleted gene data'
+        return 'Deleted gene data\n'
 
 @app.route('/genes', methods=['GET'])
 def get_genes() -> list:
@@ -48,15 +48,15 @@ def get_genes() -> list:
     """
     list_of_hgnc_ids = []
     try:
-        json_data = json.loads(rd.get('gene_data').decode('utf-8'))
+        json_data = data()
 
         for i in range(len(json_data['response'])):
-            list_of_hgnc_ids.append(json_data['response']['docs'][i]['hgnc_id']
+            list_of_hgnc_ids.append(json_data['response']['docs'][i]['hgnc_id'])
         return list_of_hgnc_ids
     except Exception as err:
-        return "No data found. Use 'curl -X POST localhost:5000/data' to fetch data"
+        return "No data found. Use 'curl -X POST localhost:5000/data' to fetch data\n"
 
-@app.route('/genes/<string: hgnc_id>', methods=['GET'])
+@app.route('/genes/<string:hgnc_id>', methods=['GET'])
 def get_genes_hgnc(hgnc_id: str) -> dict:
     """
     Args: hgnc_id - identifies which gene to pull data from
@@ -64,14 +64,14 @@ def get_genes_hgnc(hgnc_id: str) -> dict:
     Returns all data associated with <hgnc_id>
     """
     try:
-        json_data = json.loads(rd.get('gene_data').decode('utf-8'))
+        json_data = data()
         for hgnc in json_data['response']['docs']:
             if hgnc['hgnc_id'] == hgnc_id:
                 return [hgnc]
     except TypeError:
-        return "No data found. Use 'curl -X POST localhost:5000/data' to fetch data"
+        return "No data found. Use 'curl -X POST localhost:5000/data' to fetch data\n"
     except ValueError as err:
-        return "Invalid Input for: 'hgnc_id'"
+        return "Invalid Input for: 'hgnc_id'\n"
 
 def get_config():
     default_config = {"debug": True}
@@ -85,6 +85,6 @@ def get_config():
 if __name__ == '__main__':
     config = get_config()
     if config.get('debug', True):
-        app,run(debug=True, host='0.0.0.0')
+        app.run(debug=True, host='0.0.0.0')
     else:
         app.run(debug=True, host='0.0.0.0')
